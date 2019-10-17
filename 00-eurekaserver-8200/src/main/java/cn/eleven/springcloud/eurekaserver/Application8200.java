cf0040bd51316fbf5ae3b2aa35c22ee4a08f0877
liueleven <liueleven@aliyun.com>
2019-10-17 21:25:55 +0800

    eureka服务集群

---
+++ 00-eurekaserver-8200/src/main/java/cn/eleven/springcloud/eurekaserver/Application8200.java
@@ -0,0 +1,18 @@
+package cn.eleven.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @EnableEurekaServer 开启Eureka服务
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application8200 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application8200.class, args);
+    }
+
+}
